980abe8e028647743eb3923c3dbe738b15590177
sana-dodom <elmernissi@gmx.de>
2026-01-31 23:05:29 +0000

    CH9

--- componenets/ProductCard.tsx
+++ componenets/ProductCard.tsx
@@ -1,16 +1,30 @@
 "use client"
 
 import { ProductType } from "@/types"
+import { ShoppingBag, ShoppingCart } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
 
 const ProductCard = ({ product }: { product: ProductType }) => {
+    const [producTypes, setproductTypes] = useState({
+        size: product.sizes[0],
+        color: product.colors[0],
+    })
+
+    const handelProductType = ({ type, value }: { type: "size" | "color", value: string }) => {
+        setproductTypes((prev) => ({
+            ...prev,
+            [type]: value
+        }))
+
+    }
     return (
         <div className="shadow-lg rounded-lg overflow-hidden">
             {/* IMAGE */}
             <Link href={`/products/${product.id}`}>
                 <div className="relative aspect-2/3">
-                    <Image src={product.images[product.colors[0]]} alt={product.name} fill className="object-cover hover:scale-105 transition-all duration-300" />
+                    <Image src={product.images[producTypes.color]} alt={product.name} fill className="object-cover hover:scale-105 transition-all duration-300" />
                 </div>
             </Link>
             {/* PRODUCTDETAILS */}
@@ -24,12 +38,18 @@ const ProductCard = ({ product }: { product: ProductType }) => {
                     {/* SIZES */}
                     <div className="flex flex-col gap-1">
                         <span className="text-gray-500">Sizes</span>
-                        <select name="size" id="size" className="ring ring-gray-300 rounded-md px-2 py-1">
+                        <select
+                            name="size"
+                            id="size"
+                            className="ring ring-gray-300 rounded-md px-2 py-1"
+                            onChange={(e) => handelProductType({ type: "size", value: e.target.value })}
+                        >
                             {
                                 product.sizes.map(size => (
-                                    <option key={size} value ={size}>{size.toUpperCase()}</option>
+                                    <option key={size} value={size}>{size.toUpperCase()}</option>
                                 ))
                             }
+
                         </select>
                     </div>
                     {/* COLORS */}
@@ -38,12 +58,11 @@ const ProductCard = ({ product }: { product: ProductType }) => {
                         <div className="flex items-center gap-2">
                             {
                                 product.colors.map(color => (
-                                    <div className="" key={color}>
+                                    <div className={`cursor-pointer border ${producTypes.color === color ? "border-gray-800" : "border-gray-200"} rounded-full p-[1.2px]`}
+                                        key={color}
+                                        onClick={() => handelProductType({ type: "color", value: color })}>
                                         <div className="w-3.5 h-3.5 rounded-full"
-                                        style={{backgroundColor:color}}/>
-
-                                        
-
+                                            style={{ backgroundColor: color }} />
                                     </div>
                                 ))
                             }
@@ -53,7 +72,10 @@ const ProductCard = ({ product }: { product: ProductType }) => {
                 {/* PRICE AND ADD TO CART BUTTON */}
                 <div className="flex items-center justify-between">
                     <p className="font-medium">${product.price.toFixed(2)}</p>
-                    <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black">Add to Cart</button>
+                    <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-4">
+                        <ShoppingCart className="w-4 h-4" />
+                        Add to Cart
+                    </button>
                 </div>
             </div>
         </div>
