52d1630b9c0e69406290c44e28d4d6bf4d224c63
sana-dodom <elmernissi@gmx.de>
2026-02-07 12:30:06 +0000

    CH22

--- store/cartSore.tsx
+++ store/cartSore.tsx
@@ -1,11 +1,28 @@
-import { CartStoreActionsType, CartStoreStateType } from '@/types'
-import { create } from 'zustand'
+import { CartStoreActionsType, CartStoreStateType } from "@/types";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-const useCartStore = create<CartStoreStateType & CartStoreActionsType>((set) => ({
-  cart: [],
-  addToCart: (product) => set((state) => ({cart: [...state.cart, product] })),
-  removeFromCart: (product) => set((state) => ({cart: state.cart.filter((p) => p.id !== product.id)})) ,
-  clearCart: () => set({cart:[]}),
-}))
+const useCartStore = create<
+  CartStoreStateType & CartStoreActionsType
+>()(
+  persist(
+    (set) => ({
+      cart: [],
+      addToCart: (product) =>
+        set((state) => ({
+          cart: [...state.cart, product],
+        })),
+      removeFromCart: (product) =>
+        set((state) => ({
+          cart: state.cart.filter((p) => p.id !== product.id),
+        })),
+      clearCart: () => set({ cart: [] }),
+    }),
+    {
+      name: "cart",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
 
 export default useCartStore;
