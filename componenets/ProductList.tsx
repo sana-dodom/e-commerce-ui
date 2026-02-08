2e1e88afb586103b9b5f29a0b422b7937fd9cbb7
sana-dodom <elmernissi@gmx.de>
2026-02-08 17:30:32 +0100

    CH26

--- componenets/ProductList.tsx
+++ componenets/ProductList.tsx
@@ -125,7 +125,7 @@ const ProductList = ({ category, params }: { category: string, params: "homepage
                 <Categories />
             </Suspense>
             {params === "products" && <Filter/>}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-12">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  3xl:grid-cols-5 gap-12">
                 {products.map(product => (
                     <ProductCard key={product.id} product={product} />
                 ))}
